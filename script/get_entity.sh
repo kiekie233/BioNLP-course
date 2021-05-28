21811dd0fc9f104dabbc8616fe286e70372e7f32
Kiekie <56425309+kiekie233@users.noreply.github.com>
2021-05-28 10:58:33 +0800

    Add files via upload

--- script/get_entity.sh
+++ script/get_entity.sh
@@ -1,15 +1,12 @@
-#PBS -N get_covid_19_entity
-#PBS -l nodes=1:ppn=5
-#PBS -l walltime=7200:00:00
-#PBS -l mem=5G
-#PBS -q batch
-#PBS -V
-#cd $PBS_O_WORKDIR
+esearch -db pubmed -query "covid-19" | efetch -format uid > ../preparing_data/covid_19_uid.txt # 获取uid
 
 echo 'I am curating the result.\n'
 echo "\n" > covid_19_entity_result.txt
-
-for pmid in `cat ~/NLP/task3/covid_19_uid.txt`
+for pmid in `cat ../preparing_data/covid_19_uid.txt`
 do
-    curl "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator?pmids=${pmid}" >> ~/NLP/task3/covid_19_entity_result.txt
-done
+    curl "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator?pmids=${pmid}" >> ../preparing_data/covid_19_entity_result.txt
+    sleep 5.8s
+done # 通过PubTator API获取实体数据
+
+grep -E "^[0-9]{8}\s" covid_19_entity_result.txt > ../preparing_data/covid_19_entity_result_entity.txt # 提取实体数据
+grep -E "^[0-9]{8}\|a" covid_19_entity_result.txt | sed -r "s/.{11}//" > ../preparing_data/covid_19_entity_result_abstract.txt # 提取摘要数据
